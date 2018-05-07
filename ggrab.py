96aef42cc96718f07609050e2fa0f4531338b4f3
Gwen <g@10degres.net>
2018-05-07 15:01:53 +0200

    ggrab

---
+++ ggrab.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+
+from lib.google_search_results import GoogleSearchResults
+
+params = {
+    "q" : sys.argv[1],
+    "num" : "100",
+    "location" : "Austin, Texas, United States",
+    "hl" : "en",
+    "gl" : "us",
+    "google_domain" : "google.com",
+    "api_key" : "",
+}
+
+query = GoogleSearchResults( params )
+json_results = query.get_json()
+#print( ">>>> {}".format(json_results['search_information']['query']) )
+#print( ">>>> {}".format(json_results['search_information']['total_results']) )
+#print( "" )
+
+for r in json_results['organic_results']:
+    #print( "{}. {}".format(r['position'],r['link']) )
+    print( "{}. {}".format(r['position'],r['link']) )
